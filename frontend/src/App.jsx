94c5e81ef394b38898a14675e64d567b279ab186
emvalencaf <edsonmvf@gmail.com>
2023-06-16 20:57:34 -0300

    frontend initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div
+      className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row"
+    >
+      <div className="sm:flex hidden mr-10">
+        Sidebar
+      </div>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        
+      </div>
+    </div>
+  );
+};
+
+export default App;
