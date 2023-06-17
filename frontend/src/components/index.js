304335b048520f82612bd4dcf1cd0f0a750890b2
emvalencaf <edsonmvf@gmail.com>
2023-06-16 22:05:50 -0300

    create components
    - created custom button
    - created custom icon
    - created nav
    - created sidebar

---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as CustomButton } from './CustomButton';
+export { default as CustomIcon } from './CustomIcon';
+export { default as Navbar } from './Navbar';
+export { default as Sidebar } from './Sidebar';
