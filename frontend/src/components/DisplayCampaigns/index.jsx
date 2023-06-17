6b2fc20049cdf37d71b4bba9070bff7d98e258ac
emvalencaf <edsonmvf@gmail.com>
2023-06-17 18:09:28 -0300

    create components
    create Display Campaign component
    create FundCard component

---
+++ frontend/src/components/DisplayCampaigns/index.jsx
@@ -0,0 +1,94 @@
+// hooks
+import { useNavigate } from "react-router-dom";
+
+// custom components
+import { FundCard } from '../';
+
+// assets
+import { loader } from "../../assets";
+
+const DisplayCampaigns = ({
+    title = '',
+    isLoading,
+    campaigns = [],
+    isError,
+}) => {
+
+    // navigate controller
+    const navigate = useNavigate();
+
+    const handleNavigate = (campaign) => {
+        navigate(`/campaign-details/${campaign.title}`, {
+            state: campaign,
+        });
+    }
+
+    return (
+        <div
+            className=""
+        >
+            <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">
+                {title} ({campaigns.length})
+            </h1>
+            <div
+                className="flex flex-wrap mt-[20px] gap-[26px]"
+            >
+                {
+                    isLoading && (
+                        <img
+                            src={loader}
+                            atl="loader"
+                            className="
+                            w-[100px]
+                            h-[100px]
+                            object-contain
+                            "
+                        />
+                    )
+                }
+                {
+                    !isLoading && isError && (
+                        <p
+                            className="
+                            font-epilogue
+                            font-semibold
+                            text-[14px]
+                            leading-[30px]
+                            text-[#818183]
+                            "
+                        >
+                            Could not find any campany due to an internal error try again later
+                        </p>
+                    )
+                }
+                {
+                    !isLoading && !isError && campaigns.length === 0 && (
+                        <p
+                            className="
+                            font-epilogue
+                            font-semibold
+                            text-[14px]
+                            leading-[30px]
+                            text-[#818183]
+                            "
+                        >
+                            You have not created any campaigns yet
+                        </p>
+                    )
+                }
+                
+                {
+                    !isLoading && compaigns.length > 0 && campaigns.map((campaign) => (
+                        <FundCard
+                            key={campaign.id}
+                            {...campaign}
+                            handleClick={() => handleNavigate(campaign)}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+    );
+}
+
+export default DisplayCampaigns;
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,7 @@
 export { default as CustomButton } from './CustomButton';
 export { default as CustomIcon } from './CustomIcon';
+export { default as DisplayCampaigns } from './DisplayCampaigns';
 export { default as FormField } from './FormField';
+export { default as FundCard } from './FundCard';
 export { default as Navbar } from './Navbar';
 export { default as Sidebar } from './Sidebar';
